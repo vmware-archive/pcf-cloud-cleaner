70ab758a411c8203f0cc315898e7122882f40e64
Daniel Mikusa <dmikusa@pivotal.io>
2018-03-06 11:06:15 -0500

    Update version of govc that is downloaded.

--- vSphere/setup.sh
+++ vSphere/setup.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get -y install curl jq
 
 # install govc
-curl -L -O https://github.com/vmware/govmomi/releases/download/v0.14.0/govc_linux_amd64.gz
+curl -L -O https://github.com/vmware/govmomi/releases/download/v0.17.0/govc_linux_amd64.gz
 gunzip govc_linux_amd64.gz
 chmod +x govc_linux_amd64
 mv govc_linux_amd64 govc
