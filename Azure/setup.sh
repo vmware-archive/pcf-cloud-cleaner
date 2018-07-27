dca6bc06168275490118481b0cfa46cc454cfd63
Daniel Mikusa <dmikusa@pivotal.io>
2018-07-27 11:52:00 -0400

    Update to use Microsoft's new signing certificate & instructions so they work with Ubuntu 18.04.

--- Azure/setup.sh
+++ Azure/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y jq
+apt-get update
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | tee /etc/apt/sources.list.d/azure-cli.list
-apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+apt-get install -y jq curl gnupg
+
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ bionic main" | tee /etc/apt/sources.list.d/azure-cli.list
+curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 apt-get install -y apt-transport-https
-apt-get update && apt-get install -y --force-yes azure-cli
+apt-get update && apt-get install -y --allow azure-cli
